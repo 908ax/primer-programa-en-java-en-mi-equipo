6fefa30d207de8a007aa16ea7f9f185c9d8cf7b2
Leon <leonbalteriano@gmail.com>
2024-09-26 10:46:38 +0200

    Tercercommit

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,9 @@ public class Prueba {
 	public static void main(String args[]) {
 	
 		System.out.println("Garcia Naveiras");
+		System.out.println("17 años, 23/10/2006");
+		System.out.println("Pizza");
+
 	
 	}
 
