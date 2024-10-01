747e62390af9685a194200d5544120c4be01915d
Leon <leonbalteriano@gmail.com>
2024-10-01 09:53:59 +0200

    Cuarto commit

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 	
-		System.out.println("Garcia Naveiras");
 		System.out.println("17 años, 23/10/2006");
 		System.out.println("Pizza");
+		System.out.println("----");
 
 	
 	}
